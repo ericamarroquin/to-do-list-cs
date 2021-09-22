44e7c253e6d8699ba61dbde8ee20187bb9a4c3f7
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-21 20:33:43 -0700

    add passing test for item constructor and description property

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,8 +6,20 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTests
   {
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
 
-    // Test methods will go here.
-
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 }
