747e72143b16a0e3f658f13151156b83d01cb623
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-28 10:40:58 -0700

    add Model View and Controllers
    Co-authored by: Nathan Fletcher <nathan.fletcher@gmail.com>

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description, string priority)
+    {
+      Item myItem = new Item(description, priority);
+      // myItem.Priority = ;
+
+      return RedirectToAction("Index");
+    }
+  }
+}
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set; }
+    public string Priority { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
@@ -13,9 +15,23 @@ namespace ToDoList.Models
       _instances.Add(this);
     }
 
+    public Item(string description, string priority)
+      : this(description)
+      {
+        Priority = priority;
+      }
+
     public static List<Item> GetAll()
     {
-      return _instances;
+      Dictionary<string, int> priorityLookup = new Dictionary<string, int>(3)
+      {
+        {"High", 1},
+        {"Medium", 2},
+        {"Low", 3}
+      };
+
+      List<Item> sortedList = _instances.OrderBy(item => priorityLookup[item.Priority]).ToList();
+      return sortedList;
     }
 
         public static void ClearAll()
