a33e7db6f4455e3c0ec37246dac4f523a680bbef
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-30 09:40:14 -0700

    add categories object to store items of the same category

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,24 +6,36 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items")]
-    public ActionResult Index()
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+    //   List<Item> allItems = Item.GetAll();
+    //   return View(allItems);
+    // }
+
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
+      Category category = Category.Find(categoryId);
+      return View(category);
     }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+    // [HttpPost("/items")]
+    // public ActionResult Create(string description, string priority)
+    // {
+    //   Item myItem = new Item(description, priority);
+    //   return RedirectToAction("Index");
+    // }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description, string priority)
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      Item myItem = new Item(description, priority);
-      return RedirectToAction("Index");
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
 
     [HttpPost("/items/delete")]
@@ -31,13 +43,6 @@ namespace ToDoList.Controllers
     {
       Item.ClearAll();
       return View();
-    }
-
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
     }
   }
 }
